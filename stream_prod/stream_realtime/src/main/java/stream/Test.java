bb00aa866625b7e70178f12bdeeb6b5ac42f0e13
wangmiaomimidu <15308526+wangmiaomimidu@user.noreply.gitee.com>
2025-10-02 23:04:42 +0800

    1

---
+++ stream_prod/stream_realtime/src/main/java/stream/Test.java
@@ -0,0 +1,17 @@
+package stream;
+
+import lombok.SneakyThrows;
+
+/**
+ * @Package com.stream.Test
+ * @Author zhou.han
+ * @Date 2024/12/29 22:45
+ * @description:
+ */
+public class Test {
+    @SneakyThrows
+    public static void main(String[] args) {
+
+    }
+
+}
